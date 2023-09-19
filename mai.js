21a392ff372eb379391a242ff2e673f0a9564636
Yasmine135 <132566027+Yasmine135@users.noreply.github.com>
2023-09-19 07:52:14 -0300

    h

--- mai.js
+++ mai.js
@@ -1,11 +1,16 @@
-function TocaSom() {
-    document.querySelector('#som_tecla').play();
+function TocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
 }
 
 const ListaDeSons = document.querySelectorAll('.tecla');
 
+
+
+
+
 let Contador = 0;
 
+//conição
 while(Contador < 9) {
     ListaDeSons[Contador].onclick = TocaSom;
     Contador = Contador + 1;
